b96d2c02cac92cfd451cdfcd6e1d9bc25e647c93
Sumin Go <jennifer045020@gmail.com>
2017-10-30 02:06:02 +0900

    Notify Scrolled position and Change scroll's nested status

---
+++ SeoulWithYou/app/src/main/java/com/example/parktaeim/seoulwithyou/ScrollViewListener.java
@@ -0,0 +1,9 @@
+package com.example.parktaeim.seoulwithyou;
+
+/**
+ * Created by user on 2017-10-30.
+ */
+
+public interface ScrollViewListener {
+    public void onScrollChanged(MyScrollView scrollView, int x, int y, int oldx, int oldy);
+}
