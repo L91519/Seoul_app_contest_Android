31889ee07c3775d32e3563b9f7c44b98863a8ef3
taeiim <ti535666@gmail.com>
2017-11-01 20:04:15 +0900

    ChangePassword done

--- SeoulWithYou/app/src/main/java/com/example/parktaeim/seoulwithyou/Network/Service.java
+++ SeoulWithYou/app/src/main/java/com/example/parktaeim/seoulwithyou/Network/Service.java
@@ -10,6 +10,7 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
@@ -43,5 +44,10 @@ public class Service extends APIAdapter{
 
         @GET(APIUrl.GET_ART_COURSE)
         Call<JsonObject> getArtCourseList();
+
+        @FormUrlEncoded
+        @PATCH(APIUrl.CHANGE_PW_URL)
+        Call<Void> changePw(@Field("pw") String pw);
+
     }
 }
